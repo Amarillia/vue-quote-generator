0d9e5364a117994865802b62ae5a6eada88f4c22
Edina Orban <Amarillia@users.noreply.github.com>
2019-08-16 19:35:49 +0100

    updated app to pass the freecodecamp tests

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,21 @@
 <template>
   <div id="app">
-    <Header headerTitle="Random Quote Generator"/>
-    <Main msg="This is a webpage to generate some random new quotes and brighten up your day."/>
-    <Footer name="Contact"/>
-
+    <Header headerTitle="Random Quote Generator" />
+    <Main msg="Generate random new quotes and brighten up your day." />
   </div>
 </template>
 
 <script>
-import Main from "./components/AppMain.vue";
-import Footer from "./components/AppFooter.vue";
-import Header from "./components/AppHeader.vue";
+import Main from "./components/AppMain";
+import Header from "./components/AppHeader";
 
 export default {
   name: "app",
   components: {
     Main,
-    Footer,
     Header
   }
 };
-
-// var quotes = ["Don't cry because it's over, smile because it happened.", "I'm selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can't handle me at my worst, then you sure as hell don't deserve me at my best.", "Be yourself; everyone else is already taken.", "You are never too old to set another goal or to dream a new dream.", "If you can dream it, you can do it.", "Never give up, for that is just the place and time that the tide will turn.", "Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.", "Be who you are and say what you feel, because those who mind don't matter, and those who matter don't mind.", "A room without books is like a body without a soul.", "No one can make you feel inferior without your consent."];
-
-	// var author = ["- Dr. Seuss","- Marilyn Monroe","- Oscar Wilde","- C.S Lewis", "- Walt Disney", "- Harriet Beecher Stowe","- Albert Einstein","- Bernard M. Baruch","- Marcus Tullius Cicero","- Eleanor Roosevelt"];
-
-// function newQuoteGen() {
-	// var newQuote = Math.floor(Math.random() * quotes.length);
-	// var newAuthor = Math.floor(Math.random() * quotes.length);
-
-	// document.getElementById('text').innerHTML = quotes[newQuote];
-	// document.getElementById('author').innerHTML = author[newAuthor];
-
-// }
-
-// newQuoteGen();
-
 </script>
 
 <style>
@@ -49,7 +29,6 @@ body {
 
 #app {
   font-family: "Roboto Condensed", sans-serif;
-  /* font-family: "Avenir", Helvetica, Arial, sans-serif; */
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
--- src/components/AppButton.vue
+++ src/components/AppButton.vue
@@ -1,26 +1,32 @@
 <template>
-  <button variant="primary" class="genButton">
-    {{ name }}
+  <button variant="primary" class="genButton Button" @click="onClick">
+    <slot>{{ name }}</slot>
   </button>
 </template>
 
 <script>
 export default {
-  name: 'Button',
+  name: "Button",
   props: {
-    name: String
+    name: String,
+    onClick: {
+      type: Function,
+      required: true
+    }
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
+<!-- https://codeburst.io/creating-reusable-components-with-vue-js-button-component-503167facfde -->
+
 <style scoped>
 .genButton {
-    width: 200px;
-    height: 50px;
-    background-color: darkgreen;
-    border: 1px solid darkgreen;
-    color: #fff;
-    padding: 10px 15px;
+  width: 200px;
+  height: 50px;
+  background-color: darkgreen;
+  border: 1px solid darkgreen;
+  color: #fff;
+  padding: 10px 15px;
 }
 </style>
